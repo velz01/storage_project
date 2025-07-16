bf8b629b27627bdff8178daab32ac9287bb543d4
velz12 <danila.ivonov.006@gmail.com>
2025-07-16 20:03:56 +0400

    refactored configs

--- src/main/java/org/velz/storagefiles/config/MinioConfiguration.java
+++ src/main/java/org/velz/storagefiles/config/MinioConfiguration.java
@@ -5,13 +5,11 @@ import io.minio.MinioClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.velz.storagefiles.properties.MinioProperties;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableRedisHttpSession
-public class ApplicationConfiguration {
+public class MinioConfiguration {
     private final MinioProperties minioProperties;
 
     @Bean
---
+++ src/main/java/org/velz/storagefiles/config/RedisConfig.java
@@ -0,0 +1,9 @@
+package org.velz.storagefiles.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisConfig {
+}
